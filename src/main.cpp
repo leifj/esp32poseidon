7f9a5a4c17ffe0731da1e4cc52a64c4c98c384bd
Leif Johansson <leifjsunet.se>
2024-10-20 17:30:55 +0200

    ota and working mux decode

--- src/main.cpp
+++ src/main.cpp
@@ -3,15 +3,24 @@
 #include <ArduinoJson.h>
 #include <WiFiManager.h>
 #include <ESPAsyncWebServer.h>
+#include <ESPAsyncHTTPUpdateServer.h>
 #include <SPIFFS.h>
 #include <SPI.h>
 
 AsyncWebServer server(80);
+ESPAsyncHTTPUpdateServer updateServer;
 
 static const int NEXT_BAND = 15;
-static const int bandPins[] = {5,18,0,16,17,4};
-static const String bandName[] = {"6","10/12/15","17/20","30/40","80","160"};
+static const int MUX_SIG = 34;
+static const int MUX_EN = 26;
+static const int MUX_S0 = 25;
+static const int MUX_S1 = 33;
+static const int MUX_S2 = 32;
+static const int MUX_S3 = 35; // bad choice - input only. not neded for this application though
+static const String bandName[] = {"160","10/12/15","17/20","6","80","30/40"};
+int bandVoltages[] = {0,0,0,0,0,0};
 int band = -1;
+unsigned long ota_progress_millis = 0;
 
 unsigned int previousMillis = 0;
 unsigned const int interval = 30*1000;
@@ -36,28 +45,75 @@ void addBool(JsonDocument *doc, String name, bool value) {
   }
 }
 
+void addIntArray(JsonDocument *doc, String name, int value[]) {
+  JsonArray doc_0 = (*doc).createNestedArray(name);
+  for (int i = 0; i < 6; i++) {
+    doc_0.add(bandVoltages[i]);
+  }
+}
+
+int readAnalog(int channel) {
+  //Serial.printf("select %d,%d,%d\n",HIGH && (channel & B00000001),HIGH && (channel & B00000010),HIGH && (channel & B00000100));
+  digitalWrite(MUX_S0,HIGH && (channel & B00000001));
+  digitalWrite(MUX_S1,HIGH && (channel & B00000010));
+  digitalWrite(MUX_S2,HIGH && (channel & B00000100));
+  digitalWrite(MUX_EN,LOW);
+  delay(10);
+  //digitalWrite(MUX_S3,HIGH && (channel & B00001000));
+  int val = analogReadMilliVolts(MUX_SIG);
+  digitalWrite(MUX_EN,HIGH);
+  //Serial.printf("channel %d = %d\n", channel, val);
+  return val;
+}
+
 void readBand() {
-    for (int i = 0; i < sizeof(bandPins)/sizeof(int); i++) {
-        if (digitalRead(bandPins[i])) {
-            band = i;
-        }
-    }
+    //Serial.println("-----");
     band = -1;
+    for (int i = 0; i < 6; i++) {
+      bandVoltages[i] = readAnalog(i);
+      if (bandVoltages[i] > 1000) {
+          band = i;
+      }
+    }
 }
 
 void getStatus(AsyncWebServerRequest *request) {
   StaticJsonDocument<1024> doc;
-  if (band > 0) {
+  if (band >= 0) {
     addString(&doc,"band",bandName[band]);
   } else {
     addString(&doc,"band","unknown");
   }
+  addIntArray(&doc,"voltages",bandVoltages);
   char buffer[1024];
   serializeJson(doc, buffer);
   
   request->send(200, "application/json", buffer);
 }
 
+void onOTAStart() {
+  // Log when OTA has started
+  Serial.println("OTA update started!");
+  // <Add your own code here>
+}
+
+void onOTAProgress(size_t current, size_t final) {
+  // Log every 1 second
+  if (millis() - ota_progress_millis > 1000) {
+    ota_progress_millis = millis();
+    Serial.printf("OTA Progress Current: %u bytes, Final: %u bytes\n", current, final);
+  }
+}
+
+void onOTAEnd(bool success) {
+  // Log when OTA has finished
+  if (success) {
+    Serial.println("OTA update finished successfully!");
+  } else {
+    Serial.println("There was an error during OTA update!");
+  }
+  // <Add your own code here>
+}
 
 void setupApi() {
   DefaultHeaders::Instance().addHeader("Access-Control-Allow-Origin", "*");
@@ -72,8 +128,16 @@ void setupApi() {
     digitalWrite(NEXT_BAND,false);
     getStatus(request);
   });
+  server.on("/api/longpress", HTTP_GET, [](AsyncWebServerRequest *request){
+    digitalWrite(NEXT_BAND,true);
+    delay(4000);
+    digitalWrite(NEXT_BAND,false);
+    getStatus(request);
+  });
   server.serveStatic("/", SPIFFS, "/").setDefaultFile("index.html");
   server.serveStatic("/static/", SPIFFS, "/");
+  updateServer.setup(&server);
+  updateServer.setup(&server,OTA_USER,OTA_PASSWORD);
   server.begin();
 }
 
@@ -119,15 +183,19 @@ void setup() {
     return;
   }
 
+  analogReadResolution(13);
+  analogSetAttenuation(ADC_ATTENDB_MAX);
   pinMode(NEXT_BAND,OUTPUT);
   digitalWrite(NEXT_BAND,false);
-  for (int pin : bandPins) {
-    pinMode(pin,INPUT_PULLDOWN);
-  }
+  pinMode(MUX_S0,OUTPUT);
+  pinMode(MUX_S1,OUTPUT);
+  pinMode(MUX_S2,OUTPUT);
+  //pinMode(MUX_S3,OUTPUT);
+  pinMode(MUX_SIG,ANALOG);
+  pinMode(MUX_EN,OUTPUT);
 
   setupApi();
 }
-
 
 void loop() {
   unsigned long currentMillis = millis();
@@ -140,5 +208,5 @@ void loop() {
     previousMillis = currentMillis;
   }
   readBand();
-  delay(1000);
+  delay(500);
 }
